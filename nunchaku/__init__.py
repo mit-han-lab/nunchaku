2de014035085e3806b9879c0b0ea03a42bc14292
muyangli <lmxyy1999@foxmail.com>
2025-06-06 16:08:46 -0700

    add __version__

--- nunchaku/__init__.py
+++ nunchaku/__init__.py
@@ -1,3 +1,4 @@
 from .models import NunchakuFluxTransformer2dModel, NunchakuSanaTransformer2DModel, NunchakuT5EncoderModel
+from .__version__ import __version__
 
-__all__ = ["NunchakuFluxTransformer2dModel", "NunchakuSanaTransformer2DModel", "NunchakuT5EncoderModel"]
+__all__ = ["NunchakuFluxTransformer2dModel", "NunchakuSanaTransformer2DModel", "NunchakuT5EncoderModel", "__version__"]
